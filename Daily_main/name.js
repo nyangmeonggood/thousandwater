8ce5533e264981705670cfb34a8ddce876d45f33
yeonsu <bananajuseyo@gmail.com>
2020-09-07 00:28:40 +0900

    200906 Daily_main

--- Daily_main/name.js
+++ Daily_main/name.js
@@ -1,20 +1,12 @@
 const nameForm = document.querySelector(".js_name .js_form"),
     nameInput = nameForm.querySelector("input"),
     welcome = document.querySelector(".js_welcome"),
-    changeNamebox = document.querySelector(".js_changeName")
+    changeNamebox = document.querySelector(".js_changeName"),
+    blankBar = document.querySelector(".bar"),
+    timeLine = document.querySelector(".timeline")
     
 const USERNAME = "userName",
     SHOW_ON = "show"
-
-var autoFocusName = true
-window.addEventListener("load", function(event){
-    if(autoFocusName){
-        setInterval(function(){
-            nameInput.focus()
-            nameInput.click()
-        },500)
-    }
-})
 
 function loadName(){
     const userName = localStorage.getItem(USERNAME)
@@ -27,16 +19,17 @@ function loadName(){
 }
 
 function whatIsYourName(){
-    nameForm.classList.add(SHOW_ON)
+    nameForm.classList.add(SHOW_ON);
+    blankBar.classList.add(SHOW_ON);
     nameForm.addEventListener("submit", submitName)
 }
 
 function submitName(event){
     event.preventDefault();
     const currentUser = nameInput.value;
+    
     rememberName(currentUser);
     showWelcome(currentUser);
-    autoFocusName = false;
 }
 
 function rememberName(text){
@@ -44,17 +37,25 @@ function rememberName(text){
 }
 
 function showWelcome(text){
+    blankBar.classList.remove(SHOW_ON);
     nameForm.classList.remove(SHOW_ON);
+    timeLine.classList.add(SHOW_ON);
     welcome.classList.add(SHOW_ON);
+
     welcome.querySelector(".js_hi").innerHTML = `안녕하세요. ${text}님!`;
     welcome.querySelector(".js_changeName").addEventListener("click",changeName)
 }
 
 function changeName(){
     welcome.classList.remove(SHOW_ON);
+    timeLine.classList.remove(SHOW_ON);
     localStorage.removeItem(USERNAME);
-    autoFocusName = true;
-    whatIsYourName()
+    whatIsYourName();
+
+    const autoFocusName = setTimeout(function(){
+        nameInput.focus();
+        console.log("hi")
+    },500)
 }
 
 function init(){
