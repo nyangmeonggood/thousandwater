6d84fd8a06eb484a1bfe8fe3bb4ffe24d43a6d0a
yeonsu <bananajuseyo@gmail.com>
2020-09-08 17:22:53 +0900

    remainTime

--- Daily_main/clock.js
+++ Daily_main/clock.js
@@ -1,6 +1,7 @@
 const clockContainer = document.querySelector(".js_clock");
 const clockTitle = clockContainer.querySelector("h1");
-const workTimeLine = document.querySelector(".timeline_ing")
+const workTimeLine = document.querySelector(".timeline_ing"),
+    remaintTime = document.querySelector(".remain_time")
 
 function setTime(){
     const date = new Date();
@@ -17,10 +18,19 @@ function setTime(){
     const workEndTime = 18*60;
     const workStartTime = 9*60;
 
+    const remainHour = `${workEndTime/60 -1 - hour}시간`
+    const remainMinute = `${60 - minute}분`;
+
     ingWidth = (hour * 60 + minute - workStartTime) / (workEndTime - workStartTime) * 100
     if(ingWidth>100){ingWidth = 100}
+
     workTimeLine.style.width = ingWidth + "%";
-    
+    remaintTime.style.left =  ingWidth + "%";
+    if(hour >= 9 && hour < 18){
+        remaintTime.innerHTML = `남은 시간은 <span>${remainHour} ${remainMinute}</span> 입니다`
+    }else{
+        remaintTime.innerHTML = `근무시간이 아닙니다!`
+    }
 }
 
 function init(){
