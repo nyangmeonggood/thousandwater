0648f1cbaf6ac9efec6e02bb73e2da9934ab3fe8
yeonsu <bananajuseyo@gmail.com>
2020-09-10 14:47:42 +0900

    tripAdvisor_layout

--- TripAdviser/scripts/calendar.js
+++ TripAdviser/scripts/calendar.js
@@ -22,6 +22,25 @@ $(function(){
         var to = $('#to').val();
 
         search(from, to);
+
+        $("#spot_list").html("")
+        $("#spot_list").addClass("active")
+        $("#greeting, #search-panel").addClass("fade")
+
+        setTimeout(function(){
+            imgSize()
+        },500)
+    })    
+
+
+    function imgSize(){
+        var imgWidth = $(".list_item_pic").parent().width()
+        var imgHeight = imgWidth * 0.6
+        $(".list_item_pic").parent().height(imgHeight)
+    }
+
+    $(window).resize(function(){
+        imgSize()
     })
 })
 
@@ -32,10 +51,30 @@ function search(from, to){
         from,
         to,
     }, function(r){
-        console.log(r)
+        var $list = $("#spot_list");
+
+        for(var i = 0; i < r.length; i++){
+            if(i != 1){
+                var data = r[i];
+                var $item = createSpotList(data);
+
+                $list.append($item)
+            }
+        }
     })
 }
 
-function createSpotList(){
-    var $tmpl = $('')
+function createSpotList(data){
+    var $tmpl = $('#list_item-template').clone().removeAttr("id");
+
+    $tmpl.find(".list_item_pic").attr("src",data.titleImageUrl);
+    $tmpl.find(".list_item_name").html(data.name);
+    $tmpl.find(".list_item_city_name").html(data.cityName);
+
+    $tmpl.click(function(e){
+        var url = `detail.html?id=` + data.id;
+        window.location = url;
+    });
+
+    return $tmpl;
 }
--- TripAdviser/scripts/scroll.js
+++ TripAdviser/scripts/scroll.js
@@ -1,6 +1,7 @@
 const greeting = document.getElementById("greeting"),
     searchPanel = document.getElementById("search-panel"),
-    down_arrow = document.getElementById("downArrow")
+    down_arrow = document.getElementById("downArrow"),
+    list_box = document.getElementById("spot_list")
 
 down_arrow.addEventListener('wheel', function(event){
     if (event.deltaY < 0){
@@ -18,5 +19,13 @@ searchPanel.addEventListener('wheel', function(event){
         greeting.classList.remove("active")
         searchPanel.classList.remove("active")
         down_arrow.style.display = "block"
+    }
+})
+
+list_box.addEventListener('wheel', function(event){
+    if (event.deltaY < 0 && list_box.scrollTop === 0){
+        list_box.classList.remove("active");
+        greeting.classList.remove("fade");
+        searchPanel.classList.remove("fade");
     }
 })
